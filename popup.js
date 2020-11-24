2fd570470577db6a3b2c4c0ea638365bb0ee19b4
Isaiah Odhner <isaiahodhner@gmail.com>
2020-11-24 17:41:04 -0500

    Revitalize image support by bundling images
    Fix link rot and make it future proof by bundling image files instead of relying on random URLs on the web.
    
    Got a fresh batch of images from google images, removed duplicates with a tool called findimagedupes and a script similar to:
    
    	mkdir ToDelete
    
    	function removeone {
    		size1=$(stat -c%s "${file1}")
    		size2=$(stat -c%s "${file2}")
    		smaller=$file1
    		larger=$file2
    		if [[ "$size2" -lt "$size1" ]]; then
    			smaller=$file2
    			larger=$file1
    		fi
    		#mv "$larger" "$smaller.bigger" # for comparison in file browser, with alphabetical sorting
    		mv "$larger" ToDelete # for safety
    		#rm "$larger" # immediate delete
    	}
    
    findimagedupes supports scripting, but I just hacked together a script using multi-line editing to manipulate its output into a series of bash function calls like so:
    
    	file1="/home/io/Projects/sbahjifier/many-images/images47.jpg"
    	file2="/home/io/Projects/sbahjifier/many-images/sbahj.png"
    	removeone
    	file1="/home/io/Projects/sbahjifier/many-images/images6.jpg"
    	file2="/home/io/Projects/sbahjifier/many-images/5N2N48C.jpg"
    	removeone
    
    and manually handling three-way cases (image triplicates).
    And then removed a bunch of them manually, but probably not enough of them.

--- popup.js
+++ popup.js
@@ -7,18 +7,27 @@ function getCSS(selector){
 	if((Math.random() < 0.05)){
 		css += "{-webkit-appearance:media-mute-button}";
 	}else{
-		css += "{-webkit-filter: "+
+		css += "{filter: "+
 			"saturate("+Math.floor((Math.random()*7055545))+"%) "+
 			"hue-rotate("+Math.floor((Math.random()*360))+"deg) "+
 			((Math.random()<0.5)?"invert() ":"")+
 		" !important;}";
+		// css += "{}";
 	}
 	if(selector != "body"){
 		css += getCSS("body");
 	}
-	css += '*{font-family:"Comic Sans", "Comic Sans MS", "Ugly", comic, cursive;}';
-	css += ':nth-child('+Math.floor(Math.random()*10)+'):hover:after{content:url("'+getgeromyurl()+'")}';
-	css += ':nth-child('+Math.floor(Math.random()*10)+'):hover:after{content:url("'+getgeromyurl()+'")}';
+	css += `
+* {
+	font-family: "Comic Sans", "Comic Sans MS", Chalkboard SE", "Comic Neue", comic, sans-serif;
+}
+@font-face {
+	font-family: "Comic Sans";
+	src: url("comic-sans.woff");
+}
+:nth-child(${Math.floor(Math.random()*10)}):hover:after { content: url("${getgeromyurl()}"); }
+:nth-child(${Math.floor(Math.random()*10)}):hover:after { content: url("${getgeromyurl()}"); }
+`;
 	return css;
 }
 
@@ -50,4 +59,9 @@ document.head.appendChild(s);
 //////////////////////////////////////////////////////////////////
 
 function choose(arr){return arr[Math.floor(Math.random()*arr.length)];}
-function getgeromyurl(){return choose(["http://img689.imageshack.us/img689/5536/007py.png","http://t1.gstatic.com/images?q=tbn:ANd9GcTFRea_ihRIqrmpuOjE3dmey8nuqME32blpLTtoJo7uhHbWbkQ3Wo2wIpDG","https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcRYckp8nNdN-u9rg0GXxj0sQOLETLgTNoRaC5oOlEeXKNugvH8b","https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSqjPc5dA13zH5iHQ3AzQR5HtrElElt7RWjQvIRfy4JFCx_p4oFTg","http://files-cdn.formspring.me/profile/20110331/n4d9463f8e186c_medium.jpg"]);}
+function getgeromyurl(){return (typeof browser !== "undefined" ? browser : chrome).runtime.getURL(choose([
+	"many-images/gerome.jpeg",
+	"many-images/gerome-dance.gif",
+	"many-images/jerome.jpg",
+	"many-images/jerome-spin.gif",
+]));}
