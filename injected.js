793f44e94a35ecdcb24b55892bcead255edce037
Isaiah Odhner <isaiahodhner@gmail.com>
2020-11-24 18:09:25 -0500

    Format code
    Goodbye purposefully shitty HTML formatting, I'll mess you.

--- injected.js
+++ injected.js
@@ -1,72 +1,72 @@
 
 
-a=document.querySelectorAll("img");
-for(i in a){
-	if(Math.random()<0.01){
-		a[i].src=getimageurl(a[i].width,a[i].height);
-	}
-	if(Math.random()<0.01){
-		//a[i].style.webkitAppearance="media-mute-button";
-		//s=document.createElement("style");s.textContent="*{-webkit-appearance:media-mute-button;}";
-	}
+a = document.querySelectorAll("img");
+for (i in a) {
+    if (Math.random() < 0.01) {
+        a[i].src = getimageurl(a[i].width, a[i].height);
+    }
+    if (Math.random() < 0.01) {
+        //a[i].style.webkitAppearance="media-mute-button";
+        //s=document.createElement("style");s.textContent="*{-webkit-appearance:media-mute-button;}";
+    }
 }
 
 // add things to the page, mess with some css
-a=document.querySelectorAll("*");
-for(i in a){
-	try{
-		if(a[i] && a[i].nodeName && isChillElementType(a[i].nodeName) && a[i] && a[i].style && a[i].insertAdjacentHTML){
-			if(Math.random()<0.001){
-				a[i].style.backgroundImage="url("+getimageurl()+")";
-			}else if(Math.random()<0.01){
-				a[i].style.backgroundColor='#'+(Math.random()*0xFFFFFF<<0).toString(16);
-				a[i].style.color='#'+(Math.random()*0xFFFFFF<<0).toString(16);
-			}
-			//TODO: Make number of images varry inversely with the number of elements on the page.
-			if(Math.random()<0.002 || ispop){
-				a[i].insertAdjacentHTML(choose(['after','before'])+choose(['begin','end']), 
-					'<img src="'+getimageurl()+'" class="SBAHJ"'
-				+'>');
-			}else if(Math.random()<0.005 || ispop){
-				a[i].insertAdjacentHTML(choose(['after','before'])+choose(['begin','end']), 
-					'<img src="'+getimageurl()+'" class="SBAHJ" '
-					+'width="'+Math.random()*600+'" height="'+Math.random()*600+'"'
-				+'>');
-			}else if(Math.random()<0.1){
-				//a[i].style.webkitAppearance="media-mute-button";
-			}
-		}
-	} catch(err){
-		//...
-	}
+a = document.querySelectorAll("*");
+for (i in a) {
+    try {
+        if (a[i] && a[i].nodeName && isChillElementType(a[i].nodeName) && a[i] && a[i].style && a[i].insertAdjacentHTML) {
+            if (Math.random() < 0.001) {
+                a[i].style.backgroundImage = "url(" + getimageurl() + ")";
+            } else if (Math.random() < 0.01) {
+                a[i].style.backgroundColor = '#' + (Math.random() * 0xFFFFFF << 0).toString(16);
+                a[i].style.color = '#' + (Math.random() * 0xFFFFFF << 0).toString(16);
+            }
+            //TODO: Make number of images varry inversely with the number of elements on the page.
+            if (Math.random() < 0.002 || ispop) {
+                a[i].insertAdjacentHTML(choose(['after', 'before']) + choose(['begin', 'end']),
+                    '<img src="' + getimageurl() + '" class="SBAHJ"'
+                    + '>');
+            } else if (Math.random() < 0.005 || ispop) {
+                a[i].insertAdjacentHTML(choose(['after', 'before']) + choose(['begin', 'end']),
+                    '<img src="' + getimageurl() + '" class="SBAHJ" '
+                    + 'width="' + Math.random() * 600 + '" height="' + Math.random() * 600 + '"'
+                    + '>');
+            } else if (Math.random() < 0.1) {
+                //a[i].style.webkitAppearance="media-mute-button";
+            }
+        }
+    } catch (err) {
+        //...
+    }
 }
 /*
 // add things to the page
 a=document.querySelectorAll("div");
 for(i in a){
-	if(Math.random()<0.02){
-		a[i].insertAdjacentHTML('afterend', '<img src='+getimageurl()+'>');
-	}else if(Math.random()<0.005){
-		a[i].insertAdjacentHTML('afterend', '<img src='+getimageurl()+' width='+Math.random()*100+' height='+Math.random()*100+'>');
-	}else if(Math.random()<0.1){
-		a[i].style.webkitAppearance="media-mute-button";
-	}
+    if(Math.random()<0.02){
+        a[i].insertAdjacentHTML('afterend', '<img src='+getimageurl()+'>');
+    }else if(Math.random()<0.005){
+        a[i].insertAdjacentHTML('afterend', '<img src='+getimageurl()+' width='+Math.random()*100+' height='+Math.random()*100+'>');
+    }else if(Math.random()<0.1){
+        a[i].style.webkitAppearance="media-mute-button";
+    }
 }*/
 
 /////////////////////////////////////
 
-var nl=(document.getElementsByClassName("SBAHJ-static-canvas"));
-for(i in nl){buttun(nl[i],nl[i].text);}
-var nl=(document.querySelectorAll("button,h1,h2,h3,.button,input[type=submit]"));
-for(i in nl){buttun(nl[i]);}
+var nl = (document.getElementsByClassName("SBAHJ-static-canvas"));
+for (i in nl) { buttun(nl[i], nl[i].text); }
+var nl = (document.querySelectorAll("button,h1,h2,h3,.button,input[type=submit]"));
+for (i in nl) { buttun(nl[i]); }
 /*var nl=(document.getElementsByTagName("button"));
 for(i in nl){buttun(nl[i]);}
 var nl=(document.getElementsByTagName("h1"));
 for(i in nl){buttun(nl[i]);}*/
 
-function buttun(e,text){
-    if(!e || !e.style) return;
-    text=text||e.innerText||e.value||e.text||e.txt||e.title||e.name||e.className||"bro..........";
+function buttun(e, text) {
+    if (!e || !e.style) return;
+    text = text || e.innerText || e.value || e.text || e.txt || e.title || e.name || e.className || "bro..........";
     /*if(!e){
         console.log("Nothing here!");
         return;
@@ -77,16 +77,16 @@ function buttun(e,text){
         }
     }*/
     //var e=document.getElementById("buttun");
-    var canvas=document.createElement("canvas");
-    canvas.className="SBAHJ-static-canvas";
-    canvas.text=text;
+    var canvas = document.createElement("canvas");
+    canvas.className = "SBAHJ-static-canvas";
+    canvas.text = text;
     //canvas.style.border="1px solid #d3d3d3";
     //canvas.style.webkitTransform="scale(2)";
     //alert(getComputedStyle(e).pixelWidth);
-    if(Math.random()>0.5)
-    	e.style.background="hsl("+Math.floor(Math.random()*360)+", 100%, 50%)";
+    if (Math.random() > 0.5)
+        e.style.background = "hsl(" + Math.floor(Math.random() * 360) + ", 100%, 50%)";
     //e.style.border="0";
-    
+
     //canvas.width=300+Math.random()*100;
     //canvas.height=30+Math.random()*30;
     //canvas.style.width=(400+Math.random()*400)+"px";
@@ -95,102 +95,102 @@ function buttun(e,text){
     //canvas.height=canvas.offsetHeight+41;
     //canvas.style.width=getComputedStyle(e).pixelWidth;
     //canvas.style.height=getComputedStyle(e).pixelHeight;
-    var s=choose([0.5,1,1,2,2,2,3,1,3,5,1,1,1,1,2,2]);
-    canvas.width=parseInt(getComputedStyle(e).pixelWidth)/s;
-    canvas.height=parseInt(getComputedStyle(e).pixelHeight)/s;
-    canvas.style.width=parseInt(getComputedStyle(e).pixelWidth)*s+"px";
-    canvas.style.height=parseInt(getComputedStyle(e).pixelHeight)*s+"px";
+    var s = choose([0.5, 1, 1, 2, 2, 2, 3, 1, 3, 5, 1, 1, 1, 1, 2, 2]);
+    canvas.width = parseInt(getComputedStyle(e).pixelWidth) / s;
+    canvas.height = parseInt(getComputedStyle(e).pixelHeight) / s;
+    canvas.style.width = parseInt(getComputedStyle(e).pixelWidth) * s + "px";
+    canvas.style.height = parseInt(getComputedStyle(e).pixelHeight) * s + "px";
     //e.parentNode.appendChild(canvas, e);
-    var ctx=canvas.getContext("2d");
+    var ctx = canvas.getContext("2d");
     ctx.save();
-    
-    for(var i=0; i<16; i++){
-        var x,y,x1,y1;
-        x=Math.random()*50;
-        y=Math.random()*50;
-        if(Math.random()<0.5){
-            x1=x;
-            y1=y;
-        }else{
-            x1=Math.random()*50;
-            y1=Math.random()*50;
+
+    for (var i = 0; i < 16; i++) {
+        var x, y, x1, y1;
+        x = Math.random() * 50;
+        y = Math.random() * 50;
+        if (Math.random() < 0.5) {
+            x1 = x;
+            y1 = y;
+        } else {
+            x1 = Math.random() * 50;
+            y1 = Math.random() * 50;
         }
-        
-        ctx.fillStyle = "hsl("+Math.floor(Math.random()*360)+", 100%, 50%)";
-        ctx.globalCompositeOperation = (Math.random()>0.5)?"xor":((Math.random()>0.5)?"copy":"lighter");
-        ctx.fillRect(x,y,canvas.width-x1-x,canvas.height-y1-y);
+
+        ctx.fillStyle = "hsl(" + Math.floor(Math.random() * 360) + ", 100%, 50%)";
+        ctx.globalCompositeOperation = (Math.random() > 0.5) ? "xor" : ((Math.random() > 0.5) ? "copy" : "lighter");
+        ctx.fillRect(x, y, canvas.width - x1 - x, canvas.height - y1 - y);
     }
-    
+
     ctx.restore();
-    ctx.font = ((Math.random()<0.5)?"normal ":"bold ")+(15+Math.random()*20)+"pt 'Comic Sans' 'Comic Sans MS'";
+    ctx.font = ((Math.random() < 0.5) ? "normal " : "bold ") + (15 + Math.random() * 20) + "pt 'Comic Sans' 'Comic Sans MS'";
     ctx.lineWidth = 5;
     ctx.strokeStyle = 'rgba(0,0,0,255)';
-    ctx.textAlign = choose(['center','left','center','left','right']);
-    ctx.textBaseline = choose(['middle','middle','middle','middle','middle','middle','ideographic','alphabetic','bottom','top']);
-    
-    ctx.fillText(text, canvas.width/2+Math.random()*20-10, canvas.height/2+Math.random()*20-10);  
-    
-    var onclick=e.onclick;
-    var href=e.href;
-    
+    ctx.textAlign = choose(['center', 'left', 'center', 'left', 'right']);
+    ctx.textBaseline = choose(['middle', 'middle', 'middle', 'middle', 'middle', 'middle', 'ideographic', 'alphabetic', 'bottom', 'top']);
+
+    ctx.fillText(text, canvas.width / 2 + Math.random() * 20 - 10, canvas.height / 2 + Math.random() * 20 - 10);
+
+    var onclick = e.onclick;
+    var href = e.href;
+
     e.parentNode.replaceChild(canvas, e);
-    if(onclick){
-    	canvas.onclick=onclick;
-    }else if(href){
-    	canvas.href=href;
-    	canvas.onclick=function(){location.href=this.href};
+    if (onclick) {
+        canvas.onclick = onclick;
+    } else if (href) {
+        canvas.href = href;
+        canvas.onclick = function () { location.href = this.href };
     }
- // it bettor now
+    // it bettor now
 }
 
 //////////////////////////////////////////////////////
 //////////////////////
 
 // this improvis our smelling and grammer.............. BTRRRRR!!!!!1.?
-var uc=false;
-findAndReplace(/./ig,function(m){
-	return m.toLowerCase();
+var uc = false;
+findAndReplace(/./ig, function (m) {
+    return m.toLowerCase();
 });
 
-findAndReplace(/[ei]/ig,function(m){
-	var c=((Math.random()>0.4)?m:choose("ei"));
-	if(m.toUpperCase()===m && uc){return c.toUpperCase();}else{return c;}
+findAndReplace(/[ei]/ig, function (m) {
+    var c = ((Math.random() > 0.4) ? m : choose("ei"));
+    if (m.toUpperCase() === m && uc) { return c.toUpperCase(); } else { return c; }
 });
-findAndReplace(/[uo]/ig,function(m){
-	var c=((Math.random()>0.4)?m:choose("uo"));
-	if(m.toUpperCase()===m && uc){return c.toUpperCase();}else{return c;}
+findAndReplace(/[uo]/ig, function (m) {
+    var c = ((Math.random() > 0.4) ? m : choose("uo"));
+    if (m.toUpperCase() === m && uc) { return c.toUpperCase(); } else { return c; }
 });
-findAndReplace(/[aeiou]/ig,function(m){
-	var c=((Math.random()>0.001)?m:choose("aeiouooooaeuiy"));
-	if(m.toUpperCase()===m && uc){return c.toUpperCase();}else{return c;}
+findAndReplace(/[aeiou]/ig, function (m) {
+    var c = ((Math.random() > 0.001) ? m : choose("aeiouooooaeuiy"));
+    if (m.toUpperCase() === m && uc) { return c.toUpperCase(); } else { return c; }
 });
-findAndReplace(/[b-df-hj-np-tvwxz]/ig,function(m){
-	var c=((Math.random()>0.001)?m:choose("wrtpsddffghjklzxcvbnm"));
-	if(m.toUpperCase()===m && uc){return c.toUpperCase();}else{return c;}
+findAndReplace(/[b-df-hj-np-tvwxz]/ig, function (m) {
+    var c = ((Math.random() > 0.001) ? m : choose("wrtpsddffghjklzxcvbnm"));
+    if (m.toUpperCase() === m && uc) { return c.toUpperCase(); } else { return c; }
 });
-findAndReplace(/[dg]/ig,function(m){
-	var c=((Math.random()>0.3)?m:choose("dg"));
-	if(m.toUpperCase()===m && uc){return c.toUpperCase();}else{return c;}
+findAndReplace(/[dg]/ig, function (m) {
+    var c = ((Math.random() > 0.3) ? m : choose("dg"));
+    if (m.toUpperCase() === m && uc) { return c.toUpperCase(); } else { return c; }
 });
-findAndReplace(/\./ig,function(m){
-	return choose([m,m,m,m,m,m,m,m,m,m,m,m,m,m,m,m,m,m,m,m," ","....",".......",".....","..........","......"]);
+findAndReplace(/\./ig, function (m) {
+    return choose([m, m, m, m, m, m, m, m, m, m, m, m, m, m, m, m, m, m, m, m, " ", "....", ".......", ".....", "..........", "......"]);
 });
-findAndReplace("their|they're|tha're","there");
-findAndReplace("'ll","'m");
+findAndReplace("their|they're|tha're", "there");
+findAndReplace("'ll", "'m");
 //findAndReplace(/[b-df-hj-np-tvwxz]/ig,function(m){return ((Math.random()>0.01)?m:choose("wrtpsdfghjklzxcvbnm"))});
 
-function isChillElementType(tagName){
-	return tagName!=="BODY"
-	    && tagName!=="HEAD"
-	    && tagName!=="HTML"
-	    && tagName!=="STYLE";
+function isChillElementType(tagName) {
+    return tagName !== "BODY"
+        && tagName !== "HEAD"
+        && tagName !== "HTML"
+        && tagName !== "STYLE";
 }
 function findAndReplace(searchText, replacement, searchNode) {
     if (!searchText || typeof replacement === 'undefined') {
         return;
     }
     var regex = typeof searchText === 'string' ?
-                new RegExp(searchText, 'g') : searchText,
+        new RegExp(searchText, 'g') : searchText,
         childNodes = (searchNode || document.body).childNodes,
         cnLength = childNodes.length,
         excludes = 'html,head,style,title,link,meta,script,object,iframe';
@@ -200,11 +200,11 @@ function findAndReplace(searchText, replacement, searchNode) {
             (excludes + ',').indexOf(currentNode.nodeName.toLowerCase() + ',') === -1) {
             arguments.callee(searchText, replacement, currentNode);
         }
-        if (currentNode.nodeType !== 3 || !regex.test(currentNode.data) ) {
+        if (currentNode.nodeType !== 3 || !regex.test(currentNode.data)) {
             continue;
         }
         var parent = currentNode.parentNode,
-            frag = (function(){
+            frag = (function () {
                 var html = currentNode.data.replace(regex, replacement),
                     wrap = document.createElement('div'),
                     frag = document.createDocumentFragment();
@@ -219,7 +219,7 @@ function findAndReplace(searchText, replacement, searchNode) {
     }
 }
 
-function choose(arr){return arr[Math.floor(Math.random()*arr.length)];}
+function choose(arr) { return arr[Math.floor(Math.random() * arr.length)]; }
 
 
 var filenames = [
@@ -322,36 +322,36 @@ var filenames = [
     "moneytile.gif",
     "moneytile.gif",
 ];
-function getimageurl(){
-    var paths = filenames.map(function(name){
+function getimageurl() {
+    var paths = filenames.map(function (name) {
         return "many-images/" + name;
     });
     var path = choose(paths);
-	return (typeof browser !== "undefined" ? browser : chrome).runtime.getURL(path);
+    return (typeof browser !== "undefined" ? browser : chrome).runtime.getURL(path);
 }
 
 /*
 // makme speld BTRRRRR!!!!!1
-findAndReplace(/t/ig,"f");
-if(Math.random()<0.1){
-	findAndReplace(/[a|e]/i,"u");
-}else if(Math.random()<0.1){
-	findAndReplace(/[e|i]/i,"o");
-}else if(Math.random()<0.1){
-	findAndReplace(/[a|e]/i,"o");
-}else if(Math.random()<0.1){
-	findAndReplace(/[j|o|u|w]/i,"y");
-}else if(Math.random()<0.5){
-	findAndReplace(/[v|b|n|m]/i,"l");
-}else{
-	findAndReplace(/[g|h]/i,"q");
+findAndReplace(/t/ig, "f");
+if (Math.random() < 0.1) {
+    findAndReplace(/[a|e]/i, "u");
+} else if (Math.random() < 0.1) {
+    findAndReplace(/[e|i]/i, "o");
+} else if (Math.random() < 0.1) {
+    findAndReplace(/[a|e]/i, "o");
+} else if (Math.random() < 0.1) {
+    findAndReplace(/[j|o|u|w]/i, "y");
+} else if (Math.random() < 0.5) {
+    findAndReplace(/[v|b|n|m]/i, "l");
+} else {
+    findAndReplace(/[g|h]/i, "q");
 }
-if(Math.random()<0.001){
-	findAndReplace(/ /g,"");
-	findAndReplace(/\w/g,"SWEEEEEEEEEEEEEEEEE");
+if (Math.random() < 0.001) {
+    findAndReplace(/ /g, "");
+    findAndReplace(/\w/g, "SWEEEEEEEEEEEEEEEEE");
 }
-if(Math.random()<0.1){
-	findAndReplace(/\s/g,"");
+if (Math.random() < 0.1) {
+    findAndReplace(/\s/g, "");
 }
 function findAndReplace(searchText, replacement, searchNode) {
     if (!searchText || typeof replacement === 'undefined') {
@@ -359,7 +359,7 @@ function findAndReplace(searchText, replacement, searchNode) {
         return;
     }
     var regex = typeof searchText === 'string' ?
-                new RegExp(searchText, 'g') : searchText,
+        new RegExp(searchText, 'g') : searchText,
         childNodes = (searchNode || document.body).childNodes,
         cnLength = childNodes.length,
         excludes = 'html,head,style,title,link,meta,script,object,iframe';
@@ -369,11 +369,11 @@ function findAndReplace(searchText, replacement, searchNode) {
             (excludes + ',').indexOf(currentNode.nodeName.toLowerCase() + ',') === -1) {
             arguments.callee(searchText, replacement, currentNode);
         }
-        if (currentNode.nodeType !== 3 || !regex.test(currentNode.data) ) {
+        if (currentNode.nodeType !== 3 || !regex.test(currentNode.data)) {
             continue;
         }
         var parent = currentNode.parentNode,
-            frag = (function(){
+            frag = (function () {
                 var html = currentNode.data.replace(regex, replacement),
                     wrap = document.createElement('div'),
                     frag = document.createDocumentFragment();
--- popup.js
+++ popup.js
@@ -2,26 +2,26 @@
 var txt = document.querySelector('#swe');
 var img = document.querySelector('#sbahjimg');
 
-function getCSS(selector){
+function getCSS(selector) {
 	var css = "";
-	if((Math.random() < 0.05)){
+	if ((Math.random() < 0.05)) {
 		css += `
 ${selector} {
 	-webkit-appearance: media-mute-button; /* try to trigger render engine bugs :) */
 }
 `;
-	}else{
+	} else {
 		css += `
 ${selector} {
 	filter:
-		saturate(${Math.floor((Math.random()*7055))}%)
-		hue-rotate(${Math.floor((Math.random()*360))}deg)
-		${((Math.random()<0.5)?"invert() ":"")}
+		saturate(${Math.floor((Math.random() * 7055))}%)
+		hue-rotate(${Math.floor((Math.random() * 360))}deg)
+		${((Math.random() < 0.5) ? "invert() " : "")}
 		!important;
 }
 `;
 	}
-	if(selector != "body"){
+	if (selector != "body") {
 		css += getCSS("body");
 	}
 	css += `
@@ -35,43 +35,45 @@ ${selector} {
 		url('${chrome.extension.getURL('comic-sans.woff')}') format('woff'),
 		url('${chrome.extension.getURL('comic-sans.ttf')}') format('truetype');
 }
-:nth-child(${Math.floor(Math.random()*10)}):hover:after { content: url("${getgeromyurl()}"); }
-:nth-child(${Math.floor(Math.random()*10)}):hover:after { content: url("${getgeromyurl()}"); }
+:nth-child(${Math.floor(Math.random() * 10)}):hover:after { content: url("${getgeromyurl()}"); }
+:nth-child(${Math.floor(Math.random() * 10)}):hover:after { content: url("${getgeromyurl()}"); }
 `;
 	return css;
 }
 
-var c = getCSS(choose(["body","div,ul","span,li,q","a,b,q,i,td","ul,a,em,q,p",":not(img)","div:first-child,ul"]));
+var c = getCSS(choose(["body", "div,ul", "span,li,q", "a,b,q,i,td", "ul,a,em,q,p", ":not(img)", "div:first-child,ul"]));
 
-chrome.tabs.insertCSS(null, {code: c}, function() {
+chrome.tabs.insertCSS(null, { code: c }, function () {
 	if (chrome.extension.lastError) {
 		txt.innerText = "Sorry bro this page thinks its cool but its not dont worry.\nBWEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE";
-		img.src=chrome.extension.getURL('rii.jpg');
+		img.src = chrome.extension.getURL('rii.jpg');
 	} else {
 		txt.innerText = "SWEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE";
-		img.src=chrome.extension.getURL('awyeahbitches.gif');
+		img.src = chrome.extension.getURL('awyeahbitches.gif');
 	}
 });
 
-chrome.tabs.executeScript(null, {file: "injected.js"},function() {
+chrome.tabs.executeScript(null, { file: "injected.js" }, function () {
 	if (chrome.extension.lastError) {
 		txt.innerText = "Sorry bro this page thinks its cool but its not dont worry.";
-		img.src=chrome.extension.getURL('rii.jpg');
+		img.src = chrome.extension.getURL('rii.jpg');
 	} else {
 		txt.innerText += "EEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE";
 	}
 });
 
-var s=document.createElement("style");
-s.textContent=getCSS("body");
+var s = document.createElement("style");
+s.textContent = getCSS("body");
 document.head.appendChild(s);
 
 //////////////////////////////////////////////////////////////////
 
-function choose(arr){return arr[Math.floor(Math.random()*arr.length)];}
-function getgeromyurl(){return (typeof browser !== "undefined" ? browser : chrome).runtime.getURL(choose([
-	"many-images/gerome.jpeg",
-	"many-images/gerome-dance.gif",
-	"many-images/jerome.jpg",
-	"many-images/jerome-spin.gif",
-]));}
+function choose(arr) { return arr[Math.floor(Math.random() * arr.length)]; }
+function getgeromyurl() {
+	return (typeof browser !== "undefined" ? browser : chrome).runtime.getURL(choose([
+		"many-images/gerome.jpeg",
+		"many-images/gerome-dance.gif",
+		"many-images/jerome.jpg",
+		"many-images/jerome-spin.gif",
+	]));
+}
